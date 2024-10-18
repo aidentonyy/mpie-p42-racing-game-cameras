9be2f1599afa3b55dd9c1ee84b82b2c72fddc33f
aidentonyy <at1989@york.ac.uk>
2024-10-18 10:39:27 +0100

    c

---
+++ Assets/Follow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = target.transform.position + offset;
+transform.position = newPos;
+    }
+}
